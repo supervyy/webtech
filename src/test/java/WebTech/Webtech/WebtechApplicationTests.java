89b207ea39d097f620aca326aa48bf9d26eed64c
supervyy <hallo>
2024-10-17 19:09:00 +0200

    initial commit

---
+++ src/test/java/WebTech/Webtech/WebtechApplicationTests.java
@@ -0,0 +1,13 @@
+package WebTech.Webtech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebtechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
